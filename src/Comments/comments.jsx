dce3eda34ee29aa4f0ea153482be8bee17817c4b
rasul-qutbiddinov <rasulqutbiddinov@gmail.com>
2025-03-10 05:03:27 -0700

    ..

--- src/Comments/comments.jsx
+++ src/Comments/comments.jsx
@@ -3,13 +3,15 @@ import TestimonialsCard from "../Home/testimonials";
 
 const Comments = () => {
   return (
-    <div className=" mt-16 mb-0 pb-0 bg-[#012B3D] text-white py-24 px-8">
-      <div className=" text-center ">
-        <h2 className="text-8xl font-bold text-white">
+    <div className="mt-16 mb-0 pb-0 bg-[#012B3D] text-white py-12 md:py-24 px-4 md:px-8">
+      <div className="text-center">
+        <h2 className="text-4xl md:text-6xl xl:text-8xl font-bold text-white">
           COMMENTS
         </h2>
       </div>
+      <div className="max-w-[1200px] mx-auto px-4 md:px-8">
         <TestimonialsCard />
+      </div>
     </div>
   );
 };
